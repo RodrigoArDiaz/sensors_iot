e51b1788357408de5a37fa82f1548b90b1e7b79a
Diaz Rodrigo Armando <diazrodrigoar@email.com>
2025-07-24 23:26:08 -0300

    refactor: agrega configuracion para despliegue en render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Configuracion para despliegue en Render.com (peristencia de db)
+        if (app()->environment('production')) {
+            $sqlitePath = database_path('database.sqlite');
+            $tempPath = '/tmp/database.sqlite';
+    
+            if (!file_exists($tempPath) && file_exists($sqlitePath)) {
+                copy($sqlitePath, $tempPath);
+            }
+    
+            config(['database.connections.sqlite.database' => $tempPath]);
+        }
     }
 }
